2dd7c7a9a8c65e7f848b32f8ad1739d8755448a8
MarcoAgosta <agostamarco@libero.it>
2023-01-23 15:16:57 +0100

    Milestone 6

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    function index(){
+
+
+        return view('home');
+    }
+}
